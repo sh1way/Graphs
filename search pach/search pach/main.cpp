d9059b5d9880895855d227ddc727d697430aebcc
sh1way <fhvygibi@gmail.com>
2024-12-04 02:42:17 +0300

    final version graph

--- search pach/search pach/main.cpp
+++ search pach/search pach/main.cpp
@@ -13,26 +13,11 @@ using namespace std;
 using namespace sf;
 
 int** createG(int size) {
-    int** G = (int**)malloc(size * sizeof(int*));
+    int** G = new int* [size];
     for (int i = 0; i < size; i++) {
-        G[i] = (int*)malloc(size * sizeof(int));
-    }
-
-    for (int i = 0; i < size; i++) {
-        for (int j = i; j < size; j++) {
-            if (i == j) {
-                G[i][j] = 0;
-            }
-            else {
-                if (rand() % 2 == 0) {
-                    G[i][j] = rand() % 7 + 1;
-                    G[j][i] = G[i][j];
-                }
-                else {
-                    G[i][j] = 0;
-                    G[j][i] = 0;
-                }
-            }
+        G[i] = new int[size];
+        for (int j = 0; j < size; j++) {
+            G[i][j] = (i != j && rand() % 2 == 0) ? rand() % 7 + 1 : 0;
         }
     }
     return G;
@@ -47,29 +32,108 @@ void printG(int** G, int size) {
     }
 }
 
-int sumEdges(int** G, int size, int& edges) {
-    int edge = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0;j < size; j++) {
-            if (G[i][j] == 1) {
-                edge++;
-            }
-        }
-    }
-    edges = edge / 2;
-    return edges;
-}
-
 Color getEdgeColor(int weight) {
     switch (weight) {
-    case 1: return sf::Color(255, 0, 0);
-    case 2: return sf::Color(255, 69, 0);
-    case 3: return sf::Color(255, 255, 0);
-    case 4: return sf::Color(0, 255, 0);
-    case 5: return sf::Color(0, 255, 255);
-    case 6: return sf::Color(0, 0, 255);
-    case 7: return sf::Color(128, 0, 128);
-    default: return sf::Color::White;
+    case 1: return Color(255, 0, 0);
+    case 2: return Color(255, 69, 0);
+    case 3: return Color(255, 255, 0);
+    case 4: return Color(0, 255, 0);
+    case 5: return Color(0, 255, 255);
+    case 6: return Color(0, 0, 255);
+    case 7: return Color(128, 0, 128);
+    default: return Color::White;
+    }
+}
+
+void drawArrow(RenderWindow& window, float x1, float y1, float x2, float y2, Color color, float offsetX = 0, float offsetY = 0) {
+    x1 += offsetX;
+    y1 += offsetY;
+    x2 += offsetX;
+    y2 += offsetY;
+
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float length = sqrt(dx * dx + dy * dy);
+    float angle = atan2(dy, dx);
+
+    Vertex edge[] = {
+        Vertex(Vector2f(x1, y1), color),
+        Vertex(Vector2f(x2, y2), color)
+    };
+    window.draw(edge, 2, Lines);
+
+    float arrowSize = 10.0f;
+    ConvexShape arrow;
+    arrow.setPointCount(3);
+    arrow.setPoint(0, Vector2f(0, 0));
+    arrow.setPoint(1, Vector2f(-arrowSize, -arrowSize / 2));
+    arrow.setPoint(2, Vector2f(-arrowSize, arrowSize / 2));
+    arrow.setFillColor(color);
+    arrow.setPosition(x2 - dx / length * 10, y2 - dy / length * 10);
+    arrow.setRotation(angle * 180.0f / M_PI);
+
+    window.draw(arrow);
+}
+
+void createNodes(vector<CircleShape>& smallCircles, vector<Text>& labels, const Vector2f& center, int count, float radius, float smallCircleRadius, const Font& font) {
+    for (int i = 0; i < count; ++i) {
+        CircleShape smallCircle(smallCircleRadius);
+        smallCircle.setFillColor(Color(127, 255, 212));
+        float angle = i * (2 * M_PI / count);
+        float x = center.x + radius * cos(angle);
+        float y = center.y + radius * sin(angle);
+        smallCircle.setOrigin(smallCircleRadius, smallCircleRadius);
+        smallCircle.setPosition(x, y);
+        smallCircles.push_back(smallCircle);
+
+        Text label;
+        label.setFont(font);
+        label.setString(to_string(i + 1));
+        label.setCharacterSize(20);
+        label.setFillColor(Color::Red);
+        FloatRect textBounds = label.getLocalBounds();
+        label.setOrigin(textBounds.width / 2.0f, textBounds.height / 2.0f);
+        label.setPosition(x - 2, y - 6);
+        labels.push_back(label);
+    }
+}
+
+void drawMenu(RenderWindow& window, const Font& font) {
+    float menuX = 32;
+    float menuY = 70;
+    float spacing = 40;
+
+    Text colorInf;
+    colorInf.setFont(font);
+    colorInf.setString(L"Ребра и их вес");
+    colorInf.setCharacterSize(16);
+    colorInf.setPosition(28, 32);
+    colorInf.setFillColor(Color::White);
+
+    window.draw(colorInf);
+
+    for (int weight = 1; weight <= 7; ++weight) {
+        CircleShape colorCircle(8);
+        colorCircle.setFillColor(getEdgeColor(weight));
+        colorCircle.setPosition(menuX, menuY + (weight - 1) * spacing);
+
+        Text dashText;
+        dashText.setFont(font);
+        dashText.setString(" - ");
+        dashText.setCharacterSize(16);
+        dashText.setFillColor(Color::White);
+        dashText.setPosition(menuX + 20, menuY + (weight - 1) * spacing - 4);
+
+        Text weightText;
+        weightText.setFont(font);
+        weightText.setString(to_string(weight));
+        weightText.setCharacterSize(16);
+        weightText.setFillColor(Color::White);
+        weightText.setPosition(menuX + 50, menuY + (weight - 1) * spacing - 4);
+
+        window.draw(colorCircle);
+        window.draw(weightText);
+        window.draw(dashText);
     }
 }
 
@@ -77,112 +141,41 @@ int main() {
     srand(time(NULL));
     setlocale(LC_ALL, "Russian");
 
-    int nG = 10;
-    int edges = 0;
+    const int nG = 7;
     int** G = createG(nG);
-
     printG(G, nG);
-    sumEdges(G, nG, edges);
 
-    cout << "\n" << edges << endl;
-
-    RenderWindow window(VideoMode(1400, 800), "123");
-
+    RenderWindow window(VideoMode(1400, 800), "Graf");
     Font font;
-    if (!font.loadFromFile("menlo.ttf")) {
-        return -1;
-    }
-    
+    if (!font.loadFromFile("menlo.ttf")) return -1;
+
     const float mainRadius = 300.0f;
-    const int circleCount = nG;
-    const float smallCircleRadius = 20.0f;
-
-    CircleShape mainCircle(mainRadius);
-    mainCircle.setFillColor(Color::Transparent);
-    mainCircle.setOutlineColor(Color::White);
-    mainCircle.setOutlineThickness(2);
-
-    Vector2u windowSize = window.getSize();
-    Vector2f center(windowSize.x / 2.0f, windowSize.y / 2.0f);
-    mainCircle.setOrigin(mainRadius, mainRadius);
-    mainCircle.setPosition(center);
+    const float smallCircleRadius = 10.0f;
+    Vector2f center(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
 
     vector<CircleShape> smallCircles;
     vector<Text> labels;
-
-    //left panel
-    Text colorInf;
-    colorInf.setFont(font);
-    colorInf.setString(L"Ребра и их вес");
-    colorInf.setCharacterSize(16);
-    colorInf.setPosition(32, 32);
-    colorInf.setFillColor(Color::White);
-
-    for (int i = 0; i < circleCount; ++i) {
-        CircleShape smallCircle(smallCircleRadius);
-        smallCircle.setFillColor(Color(127, 255, 212));
-
-        float angle = i * (2 * M_PI / circleCount);
-
-        float x = center.x + mainRadius * cos(angle);
-        float y = center.y + mainRadius * sin(angle);
-
-        smallCircle.setOrigin(smallCircleRadius, smallCircleRadius);
-        smallCircle.setPosition(x, y);
-        smallCircles.push_back(smallCircle);
-
-        Text label;
-
-        label.setFont(font);
-        label.setString(to_string(i + 1));
-        label.setCharacterSize(20);
-        label.setFillColor(Color::Red);
-
-        FloatRect textBounds = label.getLocalBounds();
-        label.setOrigin(textBounds.width / 2.0f, textBounds.height / 2.0f);
-        label.setPosition(x - 2, y - 6);
-
-        labels.push_back(label);
-    }
+    createNodes(smallCircles, labels, center, nG, mainRadius, smallCircleRadius, font);
 
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
+            if (event.type == Event::Closed) window.close();
         }
 
         window.clear();
 
         for (int i = 0; i < nG; ++i) {
-            for (int j = i + 1; j < nG; ++j) {
+            for (int j = 0; j < nG; ++j) {
                 if (G[i][j] > 0) {
                     float angle1 = i * (2 * M_PI / nG);
                     float x1 = center.x + mainRadius * cos(angle1);
                     float y1 = center.y + mainRadius * sin(angle1);
-
-                    // Координаты второй вершины
                     float angle2 = j * (2 * M_PI / nG);
                     float x2 = center.x + mainRadius * cos(angle2);
                     float y2 = center.y + mainRadius * sin(angle2);
-
-                    // Вычисляем длину и угол ребра
-                    float dx = x2 - x1;
-                    float dy = y2 - y1;
-                    float length = sqrt(dx * dx + dy * dy);
-                    float angle = atan2(dy, dx) * 180 / M_PI; // Угол в градусах
-
-                    // Создаём ребро в виде прямоугольника
-                    sf::RectangleShape edge(sf::Vector2f(length, 3.0f)); // Толщина = 3
-                    edge.setPosition(x1, y1); // Начальная точка ребра
-                    edge.setRotation(angle); // Угол наклона ребра
-
-                    // Устанавливаем цвет ребра
-                    edge.setFillColor(getEdgeColor(G[i][j]));
-
-                    // Отрисовываем ребро
-                    window.draw(edge);
+                    float offsetX = (i < j) ? 5.0f : (i > j ? -5.0f : 0);
+                    drawArrow(window, x1, y1, x2, y2, getEdgeColor(G[i][j]), offsetX, 0);
                 }
             }
         }
@@ -192,42 +185,10 @@ int main() {
             window.draw(labels[i]);
         }
 
-        window.draw(colorInf);
-
-        // Отрисовка меню слева
-        float menuX = 32;  // Положение меню по X
-        float menuY = 70;  // Начальная Y-позиция для первого элемента
-        float spacing = 40; // Отступ между элементами меню
-        for (int weight = 1; weight <= 7; ++weight) {
-            // Создаём круг
-            CircleShape colorCircle(8); // Радиус = 8 для маленьких кругов
-            colorCircle.setFillColor(getEdgeColor(weight)); // Устанавливаем цвет в зависимости от веса
-            colorCircle.setPosition(menuX, menuY + (weight - 1) * spacing); // Расположение по Y с отступом
-
-            Text dashText;
-            dashText.setFont(font);
-            dashText.setString(" - ");
-            dashText.setCharacterSize(16);
-            dashText.setFillColor(Color::White);
-            dashText.setPosition(menuX + 20, menuY + (weight - 1) * spacing - 4);
-
-            // Создаём текст веса
-            Text weightText;
-            weightText.setFont(font);
-            weightText.setString(to_string(weight));
-            weightText.setCharacterSize(16);
-            weightText.setFillColor(Color::White);
-            weightText.setPosition(menuX + 50, menuY + (weight - 1) * spacing - 4); // Расположение текста рядом с кругом
-
-            // Отрисовываем элементы меню
-            window.draw(colorCircle);
-            window.draw(weightText);
-            window.draw(dashText);
-        }
+        drawMenu(window, font);
 
         window.display();
     }
-
 
     return 0;
 }
